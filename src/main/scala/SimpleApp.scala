29e5731e51d4143a00fe55042486f351f987afb0
yanicknedderhoff <yanick@brandwatch.com>
2016-06-06 15:41:31 +0200

    Optimize .gitignore, convert to SBT Project

---
+++ src/main/scala/SimpleApp.scala
@@ -0,0 +1,33 @@
+/**
+  * Created by ynedderhoff on 05.06.16.
+  */
+import org.apache.spark.mllib.feature.Word2Vec
+import org.apache.spark.{SparkConf, SparkContext}
+
+object SimpleApp {
+  def main(args: Array[String]) {
+    val logFile = "logs/log" // Should be some file on your system
+    val conf = new SparkConf().setAppName("Simple Application").setMaster("local[*]")
+    val sc = new SparkContext(conf)
+    val logData = sc.textFile(logFile, 2).cache()
+    val numAs = logData.filter(line => line.contains("a")).count()
+    val numBs = logData.filter(line => line.contains("b")).count()
+    println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
+
+    val input = sc.textFile("text8.txt").map(line => line.split(" ").toSeq)
+
+    val word2vec = new Word2Vec()
+
+    val model = word2vec.fit(input)
+
+    val synonyms = model.findSynonyms("china", 40)
+
+    for((synonym, cosineSimilarity) <- synonyms) {
+      println(s"$synonym $cosineSimilarity")
+    }
+
+    model.save(sc, "models/testmodel")
+    //val sameModel = Word2VecModel.load(sc, ",models/testmodel")
+
+  }
+}
